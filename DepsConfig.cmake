495f174a98d9dd7cbb3529b7cac854c8b70f087c
Gregwar <g.passault@gmail.com>
2016-05-18 19:49:36 +0200

    Deps w/ CMake enhanced

--- DepsConfig.cmake
+++ DepsConfig.cmake
@@ -15,12 +15,30 @@ function (deps_path library type)
 endfunction ()
 
 function (deps_add_library library)
-    # Adding includes
-    deps_path(${library} "includes")
-    include_directories (${PATHES})
+    # Check that the library was not already imported
+    if (NOT ";${DEPS_IMPORTED};" MATCHES ${library})
+        list(APPEND DEPS_IMPORTED ${library})
+        set (DEPS_IMPORTED ${DEPS_IMPORTED} PARENT_SCOPE)
 
-    # Adding links
-    deps_path(${library} "links")
-    list(APPEND DEPS_LIBRARIES ${PATHES})
-    set(DEPS_LIBRARIES ${DEPS_LIBRARIES} PARENT_SCOPE)
+        # Adding includes
+        deps_path(${library} "includes")
+        include_directories (${PATHES})
+
+        # Adding links
+        deps_path(${library} "links")
+
+        # Library w/ location
+        # Replace name, foo/bar will become foo_bar
+        string(REPLACE "/" "_" libraryName "${library}")
+
+        set (k 0)
+        foreach (libpath ${PATHES})
+            math(EXPR k "${k}+1")
+            set(libraryNameId "${libraryName}_${k}")
+            add_library(${libraryNameId} UNKNOWN IMPORTED)
+            set_property(TARGET ${libraryNameId} PROPERTY IMPORTED_LOCATION ${libpath})
+            list(APPEND DEPS_LIBRARIES ${libraryNameId})
+            set(DEPS_LIBRARIES ${DEPS_LIBRARIES} PARENT_SCOPE)
+        endforeach ()
+    endif()
 endfunction ()
