02f94de1169709ccbd8bee7823d031956fe03115
Gregwar <g.passault@gmail.com>
2016-05-19 08:47:04 +0200

    info porcelain

--- src/InfoCommand.php
+++ src/InfoCommand.php
@@ -9,30 +9,43 @@ class InfoCommand extends Command
 
     public function getDescription()
     {
-        return array('Print informations about current package');
+        return array('Print informations about current package',
+        'Passing "porcelain" as argument will output machine-readable list');
+    }
+
+    public function getUsage()
+    {
+        return 'infos [porcelain]';
     }
 
     public function run(array $arguments)
     {
         $json = $this->deps->nearestJson();
         $package = new Package(dirname($json));
-        Terminal::info("From $json\n\n");
-        Terminal::bold("* project name: ".$package->getName()."\n");
+        $porcelain = (count($arguments) && $arguments[0] == 'porcelain');
+        if (!$porcelain) {
+            Terminal::info("From $json\n\n");
+            Terminal::bold("* project name: ".$package->getName()."\n");
+        }
 
         $dependencies = $package->getDependencies();
-        if ($dependencies) {
-            Terminal::bold("* dependencies:\n");
-            foreach ($dependencies as $dep) {
-                echo "  - $dep ";
-                if ($this->deps->hasPackage($dep)) {
-                    Terminal::success("(installed)");
-                } else {
-                    Terminal::warning("(not installed)");
-                }
-                echo "\n";
-            }
+        if ($porcelain) {
+            echo implode(':', $dependencies)."\n";
         } else {
-            Terminal::bold("* no dependencies\n");
+            if ($dependencies) {
+                Terminal::bold("* dependencies:\n");
+                foreach ($dependencies as $dep) {
+                    echo "  - $dep ";
+                    if ($this->deps->hasPackage($dep)) {
+                        Terminal::success("(installed)");
+                    } else {
+                        Terminal::warning("(not installed)");
+                    }
+                    echo "\n";
+                }
+            } else {
+                Terminal::bold("* no dependencies\n");
+            }
         }
     }
 }
